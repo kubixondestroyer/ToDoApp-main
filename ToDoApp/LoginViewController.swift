c1cba677a99f7ded67449ed72c78ff04582657e6
kubixondestroyer <1chrobokkuba@gmail.com>
2025-01-16 00:20:14 +0100

    Initial Commit

---
+++ ToDoApp/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  ToDoApp
+//
+//  Created by Jakub Chrobok on 04/12/2024.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("logowanie")
+        print("testowanie")
+    }
+
+
+}
---
+++ ToDoApp/RegisterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterViewController.swift
+//  ToDoApp
+//
+//  Created by Jakub Chrobok on 04/12/2024.
+//
+
+import Foundation
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("rejestracja")
+    }
+
+
+}
