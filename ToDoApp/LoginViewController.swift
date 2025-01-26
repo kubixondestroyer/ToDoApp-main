80c9a6f17382cb6aa1f7d77740741148262c20fa
kubixondestroyer <1chrobokkuba@gmail.com>
2025-01-27 00:22:31 +0100

    Auth Update part.1

--- ToDoApp/LoginViewController.swift
+++ ToDoApp/LoginViewController.swift
@@ -4,8 +4,10 @@
 //
 //  Created by Jakub Chrobok on 04/12/2024.
 //
-
+import FirebaseCore
+import Foundation
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
@@ -17,7 +19,16 @@ class LoginViewController: UIViewController {
         print("testowanie2")
         print("testowanie3  ss")
         
+//        handle = Auth.auth().addStateDidChangeListener { auth, user in
+//          // ...
+//        }
     }
+    
+    Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+      guard let strongSelf = self else { return }
+      // ...
+    } //logowanie dotychczasowych użytkowników
+    
 
 
 }
--- ToDoApp/RegisterViewController.swift
+++ ToDoApp/RegisterViewController.swift
@@ -4,9 +4,10 @@
 //
 //  Created by Jakub Chrobok on 04/12/2024.
 //
-
+import FirebaseCore
 import Foundation
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
@@ -16,7 +17,15 @@ class RegisterViewController: UIViewController {
         print("rejestracja")
         print("rejestracja 2")
         print("testowe 3")
+        
+//        handle = Auth.auth().addStateDidChangeListener { auth, user in
+//          // ...
+//        }
     }
+    
+    Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+      // ...
+    } //rejesterowanie nowych uzytkowników
 
 
 }
